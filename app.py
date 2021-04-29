6212f649a352206bee66cb8f49e381c422d12df8
Neil Sabde <neilsabde@gmail.com>
2021-04-29 08:18:00 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
 import pandas as pd
 import plotly_express as px
 
-student_ids = [
-    "TRL_987",
-    "TRL_abc",
-    "TRL_imb",
-    "TRL_mda",
-    "TRL_mno",
-    "TRL_rst",
-    "TRL_xsl",
-    "TRL_xyz",
-    "TRL_zet",
-    "TRL_zny",
-]
-
-
 df = pd.read_csv("csv/data.csv")
 
-student_dfs = []
-for i in range(len(student_ids)):
-    student_dfs.append(df.loc[df["student_id"] == student_ids[i]])
+mean = df.groupby("level", as_index=False)["attempt"].mean()
+print(mean)
 
-answers = []
-for student_df in student_dfs:
-    answers.append(student_df.groupby("level")["attempt"].mean())
-
-for j in range(len(student_dfs)):
-    fig = px.scatter(
-        student_dfs[j], x=["Level 1", "Level 2", "Level 3", "Level 4"], y=answers[j]
-    )
-    fig.show()
+fig = px.scatter(
+    df,
+    x="student_id",
+    y="level",
+    size="attempt",
+    color="attempt",
+)
+fig.show()
